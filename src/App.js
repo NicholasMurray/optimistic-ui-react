cb603fd06f78ad1426ce64ecd16c67437f67cbf5
Nicholas Murray <murray.nicholas@gmail.com>
2018-04-21 22:18:25 +0100

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,107 @@
+import React, { Component } from 'react';
+import {Tweet} from './Tweet';
+
+const shouldFail = id => [3, 4].includes(id);
+
+// Fake request. Fail for id 3 or 4
+function likeTweetRequest(tweetId, like) {
+  console.log(`HTTP /like_tweet/${tweetId}?like=${like} (begin)`);
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldSucceed = !shouldFail(tweetId);
+      console.log(
+        `HTTP /like_tweet/${tweetId}?like=${like} (${shouldSucceed
+          ? 'success'
+          : 'failure'})`
+      );
+      shouldSucceed ? resolve() : reject();
+    }, 1000);
+  });
+}
+
+const initialState = {
+  tweets: [0, 3, 98, 0, 0].map((likes, i) => ({
+    id: i + 1,
+    likes,
+    username: `${shouldFail(i + 1) ? 'Fail' : 'Cool'}Bot${i + 1}`,
+    content: `Some really great content here (${i + 1})...`,
+  })),
+  likedTweets: [2],
+};
+
+// setState updater factory
+function setTweetLiked(tweetId, newLiked) {
+  return state => {
+    return {
+      tweets: state.tweets.map(
+        tweet =>
+          tweet.id === tweetId
+            ? {...tweet, likes: tweet.likes + (!newLiked ? -1 : 1)}
+            : tweet
+      ),
+      likedTweets: !newLiked
+        ? state.likedTweets.filter(id => id !== tweetId)
+        : [...state.likedTweets, tweetId],
+    };
+  };
+}
+
+class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = initialState;
+    this.onClickLike = this.onClickLike.bind(this);
+    this.likeRequestPending = false;
+  }
+
+  onClickLike(tweetId) {
+    const component = this;
+    console.log(`Clicked like: ${tweetId}`);
+
+    if (component.likeRequestPending) {
+      console.log('Like request already pending! Do nothing.');
+      return;
+    }
+
+    console.log(`Update state: ${tweetId}`);
+    const isLiked = component.state.likedTweets.includes(tweetId);
+    component.setState(setTweetLiked(tweetId, !isLiked));
+    component.likeRequestPending = true;
+
+    likeTweetRequest(tweetId, true)
+      .then(() => {
+        console.log(`then: ${tweetId}`);
+      })
+      .catch(() => {
+        console.error(`catch: ${tweetId}`);
+        component.setState(setTweetLiked(tweetId, isLiked));
+      })
+      .then(() => {
+         component.likeRequestPending = false;
+      });
+  };
+
+  render() {
+    const {tweets, likedTweets} = this.state;
+    return (
+      <div className="container">
+        <h3 className="text-muted text-center lead pt-2">
+          Optimistic UI Updates with React
+        </h3>
+        <div className="list-group">
+          {tweets.map(tweet => (
+            <Tweet
+              key={tweet.id}
+              tweet={tweet}
+              isLiked={likedTweets.includes(tweet.id)}
+              onClickLike={this.onClickLike}
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
